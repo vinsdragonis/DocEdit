8f96935e190ce8ed89d9527af4f2d0eb9023629b
vinsdragonis <zrexteam128@gmail.com>
2023-06-24 19:46:39 +0530

    Configured client and server to run side-by-side

--- server/index.js
+++ server/index.js
@@ -36,3 +36,12 @@ mongoose.connection.once("open", () => {
 mongoose.connection.on('error', (err) => {
     console.log(err);
 });
+
+if (['production'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
