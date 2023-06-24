bc710212863dbd218a336e056b85302442b9b1fd
vinsdragonis <zrexteam128@gmail.com>
2023-06-24 20:39:16 +0530

    Update IndividualDoc.jsx

--- client/src/components/IndividualDoc/IndividualDoc.jsx
+++ client/src/components/IndividualDoc/IndividualDoc.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router";
 import axios from "axios";
 
